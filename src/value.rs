df2e518e0c82beb42d68559d7cc33c33cb81002a
[RealKC] <16511305+RealKC@users.noreply.github.com>
2020-05-06 18:34:50 +0300

    Make Value be either a Boolean, Number or Nil, and implement ops for it

--- src/value.rs
+++ src/value.rs
@@ -1,1 +1,28 @@
-pub type Value = f64;
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub enum Value {
+    Boolean(bool),
+    Number(f64),
+    Nil,
+}
+
+impl Value {
+    pub fn is_falsey(&self) -> bool {
+        match self {
+            Value::Boolean(b) => !b,
+            Value::Nil => true,
+            _ => false,
+        }
+    }
+}
+
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Boolean(b) => write!(f, "{}", b),
+            Value::Number(n) => write!(f, "{}", n),
+            Value::Nil => write!(f, "nil"),
+        }
+    }
+}
